ce98b28903a5e7e233df3351ef9ac9b545acc6f6
Ian Andrewson <ianandrewson@gmail.com>
2020-02-13 15:17:10 -0800

    refactored state into useSelector and actions into useDispatch

--- src/reducers/moodReducers.js
+++ src/reducers/moodReducers.js
@@ -1,6 +1,10 @@
 import { DRINK_COFFEE, EAT_SNACK, TAKE_NAP, STUDY } from '../actions/moodActions.js';
 
-export default function moodReducer(state = {}, action) {
+export default function moodReducer(state = {
+  coffees: 0,
+  snacks: 0,
+  naps: 0,
+  studies: 0 }, action) {
   switch(action.type) {
     case DRINK_COFFEE: 
       return { ...state, coffees: state.coffees + 1 };
