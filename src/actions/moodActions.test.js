16f65d40e2705ca26333cd420e63cf5e13467b17
Ian Andrewson <ianandrewson@gmail.com>
2020-02-13 13:34:28 -0800

    tests written for moodActions, ready to TDD

---
+++ src/actions/moodActions.test.js
@@ -0,0 +1,21 @@
+describe('mood action tests', () => {
+  it('can create DRINK_COFFEE action', () => {
+    const action = drinkCoffee();
+    expect(action).toEqual({ type: 'DRINK_COFFEE' });
+  });
+
+  it('can create EAT_SNACK action', () => {
+    const action = eatSnack();
+    expect(action).toEqual({ type: 'EAT_SNACK' });
+  });
+
+  it('can create TAKE_NAP action', () => {
+    const action = takeNap();
+    expect(action).toEqual({ type: 'TAKE_NAP' });
+  });
+
+  it('can create STUDY action', () => {
+    const action = study();
+    expect(action).toEqual({ type: 'STUDY' });
+  });
+});
---
+++ src/reducers/moodReducers.js
@@ -0,0 +1,6 @@
+export default moodReducer(state = [], action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
